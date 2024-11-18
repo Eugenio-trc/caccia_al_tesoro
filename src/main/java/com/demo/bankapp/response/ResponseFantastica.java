9b1f4831b3af8935bad9a71bbe13e1c2e54c0115
Eugenio Turco <eugenio.turco-ext@temera.it>
2024-11-18 18:06:00 +0100

    first commit

---
+++ src/main/java/com/demo/bankapp/response/ResponseFantastica.java
@@ -0,0 +1,16 @@
+package com.demo.bankapp.response;
+
+import com.demo.bankapp.model.Wealth;
+
+import lombok.Data;
+
+@Data
+public class ResponseFantastica {
+	private Wealth wealth;
+	String siete = " stati";
+	String bravi = "! L'indizio";
+	String si = " tova";
+	String tra = " le";
+	String scrivanie = " della";
+	String sala = "riunioni :)";
+}
